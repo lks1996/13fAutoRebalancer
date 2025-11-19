02d854d3686681af6e8aae53fbd47e01cdcd329c
lks1996 <mosplee@naver.com>
2025-11-19 16:25:53 +0900

    응답에 불필요한 부분은 매핑되지 않아도 오류가 발생하지 않도록 수정.

--- src/main/java/com/autoRebalancer/Kis/Dto/OrderResponseDto.java
+++ src/main/java/com/autoRebalancer/Kis/Dto/OrderResponseDto.java
@@ -1,9 +1,11 @@
 package com.autoRebalancer.Kis.Dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderResponseDto {
     @JsonProperty("rt_cd")
     private String rtCd;    // 결과 코드
@@ -13,4 +15,7 @@ public class OrderResponseDto {
 
     @JsonProperty("msg1")
     private String msg1;    // 메시지 내용
+
+
+    // output 필드가 있어도 매핑하지 않고 무시
 }
